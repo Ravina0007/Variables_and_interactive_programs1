70819cd40cdd79830144550a7d0eb421539f30eb
Ravina <raniravina71996@gmail.com>
2024-01-27 09:42:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Variables and interactive programs
+
+#print('Hello, world!')
+
+#print("Hello, world!")
+
+#print('"Hello", said Ravina')
+
+#print("Good")
+#print("morning")
+
+print("Good\nmorning\nRavina")
